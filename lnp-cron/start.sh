52e66b7eafaa6fe64abc68b56821f9971bec3d0a
runyan1 <runyan1@staff.sina.com>
2023-05-19 18:09:47 +0800

    1

--- lnp-cron/start.sh
+++ lnp-cron/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "# 每天3点归档日志 日志保留30天" >> /etc/crontab; \
+echo "0 3 * * * root rsync -av --exclude backup /logs/ /logs/backup/\$(date +\%Y-\%m-\%d) && find /logs -not -path /logs/backup/* -type f -exec sh -c \"echo > {}\" \;" >> /etc/crontab; \
+echo "0 3 * * * root find /logs/backup -mindepth 1 -maxdepth 1 -mtime +$LOG_MAXDAY -type d -exec rm -rf {} \;" >> /etc/crontab
+
 # 加载crontab文件
-if [ -f "/html/crontab" ];then 
-    crontab /html/crontab; 
+if [ -f "/cron/crontab" ];then 
+    crontab /cron/crontab; 
 fi
 
 # 前台启动crond
-exec crond -n
+exec /usr/sbin/crond -n
--- lnp/start.sh
+++ lnp/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mkdir -p /logs/nginx /logs/php-fpm /logs/applogs /logs/cron
+chown -R www:www /logs/nginx /logs/php-fpm /logs/applogs /logs/cron
+
+#/usr/sbin/rsyslogd
 /usr/sbin/php-fpm
 exec /usr/sbin/nginx -g "daemon off;"
