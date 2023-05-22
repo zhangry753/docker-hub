32bc27d8fc5bce11c2868cbd37f99da12fc320ed
runyan1 <runyan1@staff.sina.com>
2023-05-22 12:16:52 +0800

    1

--- lnp-cron/start.sh
+++ lnp-cron/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "# 每天3点归档日志 日志保留30天" >> /etc/crontab; \
-echo "0 3 * * * root rsync -av --exclude backup /logs/ /logs/backup/\$(date +\%Y-\%m-\%d) && find /logs -not -path /logs/backup/* -type f -exec sh -c \"echo > {}\" \;" >> /etc/crontab; \
+echo "0 3 * * * root mkdir -p /logs/backup && rsync -av --exclude backup /logs/ /logs/backup/\$(date +\%Y-\%m-\%d) && find /logs -not -path */logs/backup/* -type f -exec sh -c \"echo > {}\" \;" >> /etc/crontab; \
 echo "0 3 * * * root find /logs/backup -mindepth 1 -maxdepth 1 -mtime +$LOG_MAXDAY -type d -exec rm -rf {} \;" >> /etc/crontab
 
 # 加载crontab文件
@@ -9,5 +9,8 @@ if [ -f "/cron/crontab" ];then
     crontab /cron/crontab; 
 fi
 
+# 启动rsyslogd memcached
+/usr/sbin/rsyslogd
+/usr/bin/memcached -d -u www -l 127.0.0.1
 # 前台启动crond
 exec /usr/sbin/crond -n
--- lnp/start.sh
+++ lnp/start.sh
@@ -4,5 +4,7 @@ mkdir -p /logs/nginx /logs/php-fpm /logs/applogs /logs/cron
 chown -R www:www /logs/nginx /logs/php-fpm /logs/applogs /logs/cron
 
 #/usr/sbin/rsyslogd
+#/usr/sbin/crond
 /usr/sbin/php-fpm
+/usr/bin/memcached -d -u www -l 127.0.0.1
 exec /usr/sbin/nginx -g "daemon off;"
