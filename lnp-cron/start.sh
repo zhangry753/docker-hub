3314adfd3180aa8de5b6bea98dce4e6df6471e38
runyan1 <runyan1@staff.sina.com>
2023-05-23 19:30:47 +0800

    1

--- lnp-cron/start.sh
+++ lnp-cron/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "# 每天3点归档日志 日志保留30天" >> /etc/crontab; \
-echo "0 3 * * * root mkdir -p /logs/backup && rsync -av --exclude backup /logs/ /logs/backup/\$(date +\%Y-\%m-\%d) && find /logs -not -path */logs/backup/* -type f -exec sh -c \"echo > {}\" \;" >> /etc/crontab; \
-echo "0 3 * * * root find /logs/backup -mindepth 1 -maxdepth 1 -mtime +$LOG_MAXDAY -type d -exec rm -rf {} \;" >> /etc/crontab
-
 # 加载crontab文件
 if [ -f "/cron/crontab" ];then 
     crontab /cron/crontab; 
